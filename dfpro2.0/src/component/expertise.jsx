5e184c031ba795e1f6dc53955cb52197df9317ae
M Daffa A <daffa.alghifary62@gmail.com>
2025-04-09 10:03:09 +0700

    about

--- dfpro2.0/src/component/expertise.jsx
+++ dfpro2.0/src/component/expertise.jsx
@@ -9,14 +9,14 @@ import bootstrap from '../assets/Bootstrap.png'
 import AnimatedContent from '../animation/AnimatedContent/AnimatedContent'
 
 export default function Expertise() {
-    const techStack = [
-        reactBoostrap, 
-        reactJS, 
-        solidity, 
-        tailwind, 
-        JS, 
-        bootstrap
-    ]
+    const techs = [
+        { name: "React", img: reactJS },
+        { name: "React Bootstrap", img: reactBoostrap },
+        { name: "Solidity", img: solidity },
+        { name: "Tailwind", img: tailwind },
+        { name: "JavaScript", img: JS },
+        { name: "Bootstrap", img: bootstrap },
+      ];
 
     return(
         <div className='mt-3'>
@@ -32,8 +32,8 @@ export default function Expertise() {
             <div className="tech mt-4">
                 <h1 className='text-[#6e92d6] font-semibold'>Tech Stack</h1>
 
-                <div className="flex gap-4 mt-3">
-                    {techStack.map((tech) => (
+                <div className="mt-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-3 place-items-center">
+                    {techs.map((tech, index) => (
                         <AnimatedContent
                             distance={150}
                             direction="horizontal"
@@ -45,10 +45,16 @@ export default function Expertise() {
                             scale={1.1}
                             threshold={0.2}
                         >
-                            <div className='bg-[#141e30] rounded-lg p-3'>
-                                <img src={tech} alt="" className='w-[4vw] h-[8vh]'/>
+                            <div key={index} className="group flex flex-col items-center">
+                                <div className="w-16 h-16 bg-[#111827] rounded-md flex items-center justify-center transition duration-300 hover:shadow-[0_0_15px_2px_rgba(255,255,255,0.3)]">
+                                    <img src={tech.img} alt={tech.name} className="w-10 h-10" />
+                                </div>
+                                <div className="wrap-break-word mt-2 px-2 py-1 text-xs rounded text-white bg-white/10 backdrop-blur-sm opacity-0 group-hover:opacity-100 transition-all duration-300 pointer-events-none whitespace-nowrap">
+                                    {tech.name}
+                                </div>
                             </div>
                         </AnimatedContent>
+                    
                     ))}
                 </div>
             </div>
