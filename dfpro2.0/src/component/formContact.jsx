362bafe94aabffecdaeac17602d96445e3c2a7fe
M Daffa A <daffa.alghifary62@gmail.com>
2025-04-13 18:47:36 +0700

    add: contact form

---
+++ dfpro2.0/src/component/formContact.jsx
@@ -0,0 +1,34 @@
+import '../App.css'
+import React from 'react'
+
+export default function ContactForm() {
+
+  return (
+    <form action="https://formspree.io/f/mblgwqvr" method="POST" class="w-[38vw] mx-auto rounded-xl p-8 space-y-5 border-1 border-white">
+        <h2 class="text-2xl font-semibold text-white">Send me a message 🚀</h2>
+        <p class="text-sm text-gray-500">Got a project in mind? Let's build something awesome together.</p>
+
+        <div>
+            <input type="text" id="name" name="name" required class="text-sm mt-1 block w-full rounded-md border border-gray-300 px-2 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600" placeholder="Full name*"/>
+        </div>
+
+        <div>
+            <input type="email" id="email" name="email" required class="text-sm mt-1 block w-full rounded-md border border-gray-300 px-2 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600" placeholder="Email address*"/>
+        </div>
+
+        <div>
+            <input type="text" id="subject" name="subject" class="text-sm mt-1 block w-full rounded-md border border-gray-300 px-2 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600" placeholder="Subject"/>
+        </div>
+
+        <div>
+            <label for="message" class="block text-sm font-medium text-white">Tell me more about your project*</label>
+            <textarea id="message" name="message" rows="4" required class="mt-1 block w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600    " placeholder="Write your message here..."></textarea>
+        </div>
+
+        <button type="submit" class="cursor-pointer w-full bg-slate-700 text-white py-2 px-4 rounded-md hover:bg-slate-800 transition duration-300 font-medium">
+            Send message
+        </button>
+    </form>
+
+  )
+}
--- dfpro2.0/src/pages/contact.jsx
+++ dfpro2.0/src/pages/contact.jsx
@@ -1,10 +1,22 @@
 import '../App.css'
 import Navbar from '../component/navbar'
+import ContactForm from '../component/formContact'
 
 export default function Contact() {
     return(
         <div>
             <Navbar/>
+
+            <div className='flex justify-center gap-[5rem] text-white mt-10 items-center'>
+                <div>
+                    <h1 className='text-5xl font-bold'>Text me.<br/>Tell me about your<br/>project</h1>
+                    <h1 className='mt-4'>Let's work together✨</h1>
+                </div>
+                
+                <div>
+                    <ContactForm/>
+                </div>
+            </div>
         </div>
     )
 }
