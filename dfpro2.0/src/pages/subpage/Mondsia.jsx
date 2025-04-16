d742a5b6ebb7c33343d269e44b6d17d6405b0e75
M Daffa A <daffa.alghifary62@gmail.com>
2025-04-16 19:24:26 +0700

    add: subpage project mondsia & brandin

--- dfpro2.0/src/pages/subpage/Mondsia.jsx
+++ dfpro2.0/src/pages/subpage/Mondsia.jsx
@@ -1,9 +1,52 @@
 import '../../App.css'
+import Navbar from '../../component/navbar'
+import mondsia from '../../assets/mondsia.png'
+import Back from '../../component/Back'
 
 export default function Mondsia() {
     return(
         <div>
-            <h1>Mondsia</h1>
+            <Navbar/>
+            
+            <div className='flex justify-center gap-10 my-15 text-white items-center'>
+                <div>
+                    <Back/>
+                    <img src={mondsia} alt="" className='w-[45vw] rounded-xl mt-8'/>
+                </div>
+                            
+            
+                <div className='max-w-lg'>
+                    <div className='flex items-center gap-3 mb-4'>
+                        <h1 className='font-bold text-2xl'>Mondsia</h1>
+                        <span className='text-sm font-semibold px-3 py-1 bg-sky-700 rounded-full text-white'>
+                            Individual Project
+                        </span>
+                    </div>
+                                
+                    <p className='text-md text-justify'>
+                        This project is a cultural website that showcases the rich diversity of Indonesian heritage 
+                        across the six major islands: Sumatra, Java, Kalimantan, Sulawesi, Bali, and Papua. Each island 
+                        section highlights traditional dances, unique traditional houses, and local customs or rituals 
+                        that have been passed down through generations. The goal is to provide an engaging and educational platform that allows users to explore 
+                        Indonesia’s cultural identity in a modern and interactive way.
+                    </p>
+            
+                    <h1 className='font-bold text-lg my-3'>Tech stack</h1>
+            
+                    <div className='flex gap-3'>
+                        <p className='py-1 px-3 bg-slate-700 rounded-full text-sm'>HTML</p>
+                        <p className='py-1 px-3 bg-slate-700 rounded-full text-sm'>Bootstrap</p>
+                        <p className='py-1 px-3 bg-slate-700 rounded-full text-sm'>Javascript</p>
+                    </div>
+            
+                    <div>
+                        <h1 className='font-bold text-lg my-3'>Project Link:</h1>
+                        <a href="" className='py-1 px-3 bg-red-700 rounded-full text-md'>
+                            Under Maintenance
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
