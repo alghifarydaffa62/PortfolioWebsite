b5c1bfa6f0972667677d38303f84e4a35e1b581b
M Daffa A <daffa.alghifary62@gmail.com>
2025-07-11 09:25:32 +0700

    update project link and name

--- dfpro2.0/src/component/smartContract.jsx
+++ dfpro2.0/src/component/smartContract.jsx
@@ -3,7 +3,7 @@ import AnimatedContent from '../animation/AnimatedContent/AnimatedContent'
 import FadeContent from '../animation/FadeContent/FadeContent'
 import wallet from '../assets/wallet.png'
 import charity from '../assets/charity.png'
-import auction from '../assets/auction.png'
+import crowdfunding from '../assets/crowdfunding.png'
 import escrow from '../assets/escrow.png'
 import payroll from '../assets/payroll.png'
 import voting from '../assets/voting.png'
@@ -23,7 +23,7 @@ export default function SmartContract() {
                 threshold={0.2}
             >
                 <div>
-                    <h1 className="text-center text-3xl font-bold">Smart Contract Projects</h1>
+                    <h1 className="text-3xl font-bold text-center">Smart Contract Projects</h1>
                     <p className="text-center mt-3 text-[11px] lg:text-sm text-gray-300">
                         These smart contract projects reflect my ability to build secure,<br />
                         efficient, and decentralized solutions on the blockchain.
@@ -37,63 +37,63 @@ export default function SmartContract() {
                 easing="ease-out"
                 initialOpacity={0}
             >
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 mt-8 mb-10">
+                <div className="grid grid-cols-1 gap-6 px-4 mt-8 mb-10 sm:grid-cols-2 lg:grid-cols-3">
                     {[
                         { 
                             img: wallet, 
                             title: "Secure Wallet", 
                             desc: "A blockchain-powered wallet ensuring safe and private transactions.",
-                            link: "https://github.com/alghifarydaffa62/Web3/tree/main/Project/Securewallet" 
+                            link: "https://github.com/alghifarydaffa62/Smart-Contract/tree/main/Secure-Wallet" 
                         },
                         { 
                             img: charity, 
                             title: "Charity", 
                             desc: "A transparent and safe smart contract for charity purpose",
-                            link: "https://github.com/alghifarydaffa62/Web3/tree/main/Project/Charity"
+                            link: "https://github.com/alghifarydaffa62/Smart-Contract/tree/main/Charity"
                         },
                         { 
                             img: escrow, 
                             title: "Escrow", 
                             desc: "A smart contract for secure buyer-seller transactions.",
-                            link: "https://github.com/alghifarydaffa62/Web3/tree/main/Project/Escrow"
+                            link: "https://github.com/alghifarydaffa62/Smart-Contract/tree/main/Escrow"
                         },
                         { 
                             img: voting, 
                             title: "Voting", 
                             desc: "Decentralized voting system ensuring fair and tamper-proof results.",
-                            link: "https://github.com/alghifarydaffa62/Web3/tree/main/Project/Voting" 
+                            link: "https://github.com/alghifarydaffa62/Smart-Contract/tree/main/Voting" 
                         },
                         { 
-                            img: auction, 
-                            title: "Auction", 
-                            desc: "Smart contract for secure bidding and transparent NFT auction",
-                            link: "https://github.com/alghifarydaffa62/Web3/tree/main/Project/Auction" 
+                            img: crowdfunding, 
+                            title: "CrowdFunding", 
+                            desc: "Smart contract for funding a public campaign",
+                            link: "https://github.com/alghifarydaffa62/Smart-Contract/tree/main/CrowdFunding" 
                         },
                         { 
                             img: payroll, 
                             title: "Payroll", 
                             desc: "Automates salary payments using smart contracts for reliability.",
-                            link: "https://github.com/alghifarydaffa62/Web3/tree/main/Project/Payroll"
+                            link: "https://github.com/alghifarydaffa62/Smart-Contract/tree/main/Payroll"
                         }
                     ].map((project, index) => (
                         <SpotlightCard
                             key={index}
-                            className="custom-spotlight-card p-5 rounded-xl bg-neutral-900 hover:shadow-lg transition w-full sm:w-auto"
+                            className="w-full p-5 transition custom-spotlight-card rounded-xl bg-neutral-900 hover:shadow-lg sm:w-auto"
                             spotlightColor="rgba(0, 229, 255, 0.2)"
                         >
                             <div className="">
                                 <img 
                                     src={project.img} 
                                     alt={project.title} 
-                                    className="w-12 h-12 object-contain mb-3"
+                                    className="object-contain w-12 h-12 mb-3"
                                 />
                                 <h1 className="text-lg font-semibold">{project.title}</h1>
-                                <p className="text-sm text-gray-300 mt-2 mb-4 break-words">{project.desc}</p>
+                                <p className="mt-2 mb-4 text-sm text-gray-300 break-words">{project.desc}</p>
                                 <a
                                     href={project.link}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="px-4 py-2 bg-neutral-800 rounded-lg text-sm hover:bg-neutral-700 transition"
+                                    className="px-4 py-2 text-sm transition rounded-lg bg-neutral-800 hover:bg-neutral-700"
                                 >
                                     Know more
                                 </a>
