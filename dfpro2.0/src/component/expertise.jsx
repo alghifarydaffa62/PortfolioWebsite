c96ff6d33a95f392776f53068118c5afbb8ae3b0
M Daffa A <daffa.alghifary62@gmail.com>
2025-08-16 20:28:29 +0700

    Feat: add new skill expersite

--- dfpro2.0/src/component/expertise.jsx
+++ dfpro2.0/src/component/expertise.jsx
@@ -5,27 +5,37 @@ import solidity from '../assets/Solidity.png';
 import tailwind from '../assets/Tailwind-CSS.png';
 import JS from '../assets/JavaScript.png';
 import bootstrap from '../assets/Bootstrap.png';
+import alchemy from "../assets/alchemy.jpeg";
+import zustand from "../assets/zustand.svg";
+import hardhat from "../assets/Hardhat.png";
+import foundry from "../assets/foundry.png";
+import ethers from "../assets/ethers.png";
 import FadeContent from '../animation/FadeContent/FadeContent';
 
 export default function Expertise() {
   const techs = [
     { name: "React", img: reactJS },
     { name: "React Bootstrap", img: reactBoostrap },
-    { name: "Solidity", img: solidity },
     { name: "Tailwind", img: tailwind },
     { name: "JavaScript", img: JS },
     { name: "Bootstrap", img: bootstrap },
+    { name: "Zustand", img: zustand },
+    { name: "Solidity", img: solidity },
+    { name: "Hardhat", img: hardhat },
+    { name: "Foundry", img: foundry },
+    { name: "ethers.js", img: ethers },
+    { name: "Alchemy", img: alchemy },
   ];
 
   return (
-    <div className="mt-16 mb-10 text-white px-6">
-      <div className="tech text-center max-w-6xl mx-auto">
+    <div className="px-6 mt-16 mb-10 text-white">
+      <div className="max-w-6xl mx-auto text-center tech">
         <h1 className="text-3xl font-bold">Tech Stack</h1>
-        <p className="mt-3 text-md text-gray-300">
+        <p className="mt-3 text-gray-300 text-md">
           My current tech stack for building websites and smart contracts
         </p>
 
-        <div className="mt-10 grid grid-cols-2 justify-center gap-12 lg:flex">
+        <div className="flex flex-wrap justify-center w-full mt-10 gap-x-10 gap-y-9">
           {techs.map((tech, index) => (
             <FadeContent
               key={index}
@@ -34,19 +44,19 @@ export default function Expertise() {
               easing="ease-out"
               initialOpacity={0}
             >
-              <div className="flex flex-col items-center w-[40vw] sm:w-[20vw] lg:w-auto hover:scale-105 transition duration-300">
+              <div className="flex flex-col items-center transition duration-300 hover:scale-105 w-28">
                 <img
                   src={tech.img}
                   alt={tech.name}
-                  className="w-12 h-12 object-contain mb-2"
+                  className="object-contain w-12 h-12 mb-2"
                 />
                 <p className="text-sm text-gray-400">{tech.name}</p>
               </div>
             </FadeContent>
           ))}
         </div>
+
       </div>
     </div>
   );
 }
-
